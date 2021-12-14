eceb1c97d83132a4cd7ab927dd34cf02959c7569
ttruster <timothy.truster@gmail.com>
2021-12-14 16:16:04 -0500

    Initial files

---
+++ src/base/BeaverApp.C
@@ -0,0 +1,53 @@
+#include "BeaverApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+InputParameters
+BeaverApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+
+  // Do not use legacy material output, i.e., output properties on INITIAL as well as TIMESTEP_END
+  params.set<bool>("use_legacy_material_output") = false;
+
+  return params;
+}
+
+BeaverApp::BeaverApp(InputParameters parameters) : MooseApp(parameters)
+{
+  BeaverApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+BeaverApp::~BeaverApp() {}
+
+void
+BeaverApp::registerAll(Factory & f, ActionFactory & af, Syntax & syntax)
+{
+  ModulesApp::registerAll(f, af, syntax);
+  Registry::registerObjectsTo(f, {"BeaverApp"});
+  Registry::registerActionsTo(af, {"BeaverApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+BeaverApp::registerApps()
+{
+  registerApp(BeaverApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+BeaverApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  BeaverApp::registerAll(f, af, s);
+}
+extern "C" void
+BeaverApp__registerApps()
+{
+  BeaverApp::registerApps();
+}
